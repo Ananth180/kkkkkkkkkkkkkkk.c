9c0df2da75a03dc023fe9c8fe8e2b152d00cf7b9
Ananth180 <35765113+Ananth180@users.noreply.github.com>
2018-02-12 13:38:44 +0530

    Create kkkkkkkkkkkk.c

---
+++ kkkkkkkkkkkk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ int 1,2,3,4;
+ clrscr();
+ printf("enter the string value:");
+ scanf("%d %d",/n);
+ printf("the values 1,2,3,4);
+ scanf("%d %d %d %d ,/n);
+ getch();
+ return 0;
+ }
